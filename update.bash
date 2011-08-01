ccd99d5d901721d0f0ebd4a1873afa46541a1e8b
Joe Heck <heckj@mac.com>
2011-08-01 12:49:21 -0700

    adding in zookeeper

--- update.bash
+++ update.bash
@@ -92,6 +92,7 @@ git_clone_or_pull celery git://github.com/ask/celery.git
 git_clone_or_pull kombu https://github.com/ask/kombu.git
 git_clone_or_pull git-cl https://github.com/martine/git-cl.git
 git_clone_or_pull kokki git://github.com/samuel/kokki
+git_clone_or_pull zookeeper https://github.com/apache/zookeeper.git
 
 echo " ------------------------------------------------------------------ "
 NAME=openstack.compute
