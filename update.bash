2e19882929049c825baf36da5a103e0e15f089dd
Joe Heck <heckj@mac.com>
2011-10-31 18:27:40 -0700

    keystoneclient

--- update.bash
+++ update.bash
@@ -70,6 +70,7 @@ git_clone_or_pull controller git@github.com:4P/controller.git
 git_clone_or_pull noVNC https://github.com/cloudbuilders/noVNC.git
 git_clone_or_pull devstack https://github.com/cloudbuilders/devstack.git
 git_clone_or_pull keystonelight https://github.com/termie/keystonelight.git
+git_clone_or_pull python-keystoneclient https://github.com/4P/python-keystoneclient
 
 echo " ======================================= "
 echo "            GITHUB (related)             "
