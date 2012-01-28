b26466f886fd3f290039a20422222ac597eb6afa
Joe Heck <heckj@mac.com>
2012-01-28 08:33:08 +0000

    cleanup

--- update.bash
+++ update.bash
@@ -10,6 +10,7 @@ function git_update {
     git remote -v
     git status
     git pull origin master
+    s2
   fi
 }
 
@@ -40,7 +41,7 @@ function bzr_clone_or_pull {
 }
 
 echo " ======================================= "
-echo "        GITHUB (Fourth Paradigm)         "
+echo "             GITHUB (Nebula)             "
 echo " ======================================= "
 
 echo " ------------------------------------------------------------------ "
@@ -62,17 +63,12 @@ git_clone_or_pull ops git@github.com:4P/ops.git
 git_clone_or_pull maestro git@github.com:4P/maestro.git
 git_clone_or_pull python-maestroclient git@github.com:4P/python-maestroclient.git
 git_clone_or_pull ipxe git@github.com:4P/ipxe.git
-git_clone_or_pull lcd_ctrl-fw git@github.com:4P/lcd_ctrl-fw.git
-git_clone_or_pull lcd_ctrl-pcb git@github.com:4P/lcd_ctrl-pcb.git
 git_clone_or_pull sheepdog git@github.com:4P/sheepdog.git
 git_clone_or_pull arista git@github.com:4P/arista.git
 git_clone_or_pull nebula-dashboard git@github.com:4P/nebula-dashboard.git
 git_clone_or_pull public-www git@github.com:4P/public-www.git
 git_clone_or_pull opencompute git@github.com:4P/opencompute.git
 git_clone_or_pull stashboard git@github.com:4P/stashboard.git
-
-# ??
-git_clone_or_pull controller git@github.com:4P/controller.git
 
 git_clone_or_pull python-keystoneclient git@github.com:4P/python-keystoneclient
 git_clone_or_pull python-quantumclient git@github.com:4P/python-quantumclient
@@ -82,12 +78,13 @@ git_clone_or_pull noVNC git@github.com:4P/noVNC.git
 git_clone_or_pull devstack https://github.com/openstack-dev/devstack.git
 git_clone_or_pull openstack-common git@github.com:4P/openstack-common.git
 
+git_clone_or_pull nova git@github.com:4P/nova.git
+git_clone_or_pull swift git@github.com:4P/swift.git
+git_clone_or_pull glance git@github.com:4P/glance.git
+git_clone_or_pull quantum git@github.com:4P/quantum.git
+git_clone_or_pull horizon git@github.com:4P/horizon.git
 git_clone_or_pull keystone git@github.com:4P/keystone.git
-git_clone_or_pull nova git@github.com/4P/nova.git
-git_clone_or_pull swift git@github.com/4P/swift.git
-git_clone_or_pull glance git@github.com/4P/glance.git
-git_clone_or_pull quantum git@github.com/4P/quantum.git
-git_clone_or_pull horizon git@github.com/4P/horizon.git
+
 
 git_clone_or_pull keystonelight git@github.com:termie/keystonelight.git
 git_clone_or_pull openstack-qa https://github.com/openstack-dev/openstack-qa.git
@@ -106,16 +103,16 @@ git_clone_or_pull object-api https://github.com/openstack/object-api.git
 git_clone_or_pull image-api https://github.com/openstack/image-api.git
 git_clone_or_pull identity-api https://github.com/openstack/identity-api.git
 git_clone_or_pull openstack-manuals https://github.com/openstack/openstack-manuals.git
-
-git_clone_or_pull lettuce https://github.com/gabrielfalcao/lettuce.git
 git_clone_or_pull openstack-org https://github.com/toddmorey/openstack-org.git
-git_clone_or_pull cobbler git://git.fedorahosted.org/cobbler
+
+#git_clone_or_pull lettuce https://github.com/gabrielfalcao/lettuce.git
+#git_clone_or_pull cobbler git://git.fedorahosted.org/cobbler
 git_clone_or_pull benchmarker git@github.com:heckj/benchmarker.git
 git_clone_or_pull py-statsd git@github.com:heckj/py-statsd.git
 git_clone_or_pull celery git://github.com/ask/celery.git
 git_clone_or_pull kombu https://github.com/ask/kombu.git
-git_clone_or_pull git-cl https://github.com/martine/git-cl.git
-git_clone_or_pull kokki git://github.com/samuel/kokki
+#git_clone_or_pull git-cl https://github.com/martine/git-cl.git
+#git_clone_or_pull kokki git://github.com/samuel/kokki
 git_clone_or_pull zookeeper https://github.com/apache/zookeeper.git
 git_clone_or_pull pyzk-recipes https://github.com/henryr/pyzk-recipes.git
 git_clone_or_pull zk-smoketest https://github.com/phunt/zk-smoketest.git
@@ -125,42 +122,9 @@ git_clone_or_pull fabric git@github.com:fabric/fabric.git
 git_clone_or_pull kong https://github.com/cloudbuilders/kong.git
 git_clone_or_pull stacktester https://github.com/rackspace-titan/stacktester.git
 
-git_clone_or_pull mongo-python-driver https://github.com/mongodb/mongo-python-driver.git
+#git_clone_or_pull mongo-python-driver https://github.com/mongodb/mongo-python-driver.git
 git_clone_or_pull python-evzookeeper https://github.com/maoy/python-evzookeeper
 git_clone_or_pull curator https://github.com/Netflix/curator
-
-echo " ------------------------------------------------------------------ "
-NAME=openstack.compute
-echo "src/$NAME"
-if [ -d ~/src/$NAME/.git ]; then
-  cd ~/src/$NAME
-  git remote -v
-  git status
-  git pull origin master
-  git pull cloudbuilders master
-else
-  cd ~/src
-  git clone https://github.com/jacobian/openstack.compute.git $NAME
-  git remote add cloudbuilders https://github.com/cloudbuilders/openstack.compute.git
-fi
-
-echo " ------------------------------------------------------------------ "
-echo "src/keepalived"
-NAME=keepalived
-echo "src/$NAME"
-if [ -d ~/src/$NAME/.git ]; then
-  cd ~/src/$NAME
-  git remote -v
-  git status
-  git pull origin master
-  git pull github master
-else
-  cd ~/src
-  git clone http://master.formilux.org/git/people/alex/keepalived.git/ $NAME
-  git remote add github git@github.com:heckj/keepalived.git
-fi
-
-git_clone_or_pull openstack-org https://github.com/toddmorey/openstack-org.git
 
 echo " ================================= "
 echo "         BZR (related)             "
@@ -168,16 +132,16 @@ echo " ================================= "
 
 bzr_clone_or_pull graphite lp:graphite
 bzr_clone_or_pull txzookeper lp:txzookeeper
-bzr_clone_or_pull juju lp:juju
-bzr_clone_or_pull charm-tools lp:charm-tools
-
-echo " ================================= "
-echo "            IOS & MAC              "
-echo " ================================= "
-
-git_clone_or_pull rackspace-ios https://github.com/rackspace/rackspace-ios.git
-git_update ioctocat
-git_update iTerm2
-git_update stsh-client
-git_update ludus
-git_update boardtest
+#bzr_clone_or_pull juju lp:juju
+#bzr_clone_or_pull charm-tools lp:charm-tools
+#
+#echo " ================================= "
+#echo "            IOS & MAC              "
+#echo " ================================= "
+#
+#git_clone_or_pull rackspace-ios https://github.com/rackspace/rackspace-ios.git
+#git_update ioctocat
+#git_update iTerm2
+#git_update stsh-client
+#git_update ludus
+#git_update boardtest
