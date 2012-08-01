37f6e8cc18a7b6f776627789b4c5fb41cc0896a8
Joe Heck <heckj@mac.com>
2012-08-01 19:51:49 +0000

    updating repo sets

--- update.bash
+++ update.bash
@@ -61,19 +61,10 @@ else
   git remote add devcamcar git@github.com:devcamcar/dotfiles.git
 fi
 
-git_clone_or_pull nebulapki git@github.com:4P/nebulapki.git
-git_clone_or_pull initramfs-tools git@github.com:4P/initramfs-tools.git
-git_clone_or_pull ops git@github.com:4P/ops.git
 git_clone_or_pull chef-ops git@github.com:4P/chef-ops.git
-git_clone_or_pull maestro git@github.com:4P/maestro.git
-git_clone_or_pull tools git@github.com:4P/tools.git
-git_clone_or_pull eos git@github.com:4P/eos.git
 git_clone_or_pull buildroot git@github.com:4P/buildroot.git
-git_clone_or_pull python-maestroclient git@github.com:4P/python-maestroclient.git
 git_clone_or_pull ipxe git@github.com:4P/ipxe.git
-git_clone_or_pull sheepdog git@github.com:4P/sheepdog.git
 git_clone_or_pull arista git@github.com:4P/arista.git
-git_clone_or_pull nebula-dashboard git@github.com:4P/nebula-dashboard.git
 git_clone_or_pull public-www git@github.com:4P/public-www.git
 git_clone_or_pull opencompute git@github.com:4P/opencompute.git
 git_clone_or_pull stashboard git@github.com:4P/stashboard.git
@@ -81,31 +72,42 @@ git_clone_or_pull alexandria git@github.com:4P/alexandria.git
 
 git_clone_or_pull bp-issues https://github.com/ttx/bp-issues.git
 git_clone_or_pull devstack https://github.com/openstack-dev/devstack.git
-git_clone_or_pull python-keystoneclient git@github.com:openstack/python-keystoneclient
-git_clone_or_pull python-quantumclient git@github.com:openstack/python-quantumclient
-git_clone_or_pull python-novaclient git@github.com:openstack/python-novaclient
-git_clone_or_pull python-melangeclient git@github.com:openstack/python-melangeclient
-git_clone_or_pull python-glanceclient git@github.com:openstack/python-glanceclient
-git_clone_or_pull noVNC git@github.com:openstack/noVNC.git
+git_clone_or_pull python-keystoneclient git@github.com:4P/python-keystoneclient
+git_clone_or_pull python-quantumclient git@github.com:4P/python-quantumclient
+git_clone_or_pull python-novaclient git@github.com:4P/python-novaclient
+git_clone_or_pull python-glanceclient git@github.com:4P/python-glanceclient
+git_clone_or_pull noVNC git@github.com:4P/noVNC.git
 
-git_clone_or_pull openstack-common git@github.com:openstack/openstack-common.git
-git_clone_or_pull nova git@github.com:openstack/nova.git
-git_clone_or_pull swift git@github.com:openstack/swift.git
-git_clone_or_pull glance git@github.com:openstack/glance.git
-git_clone_or_pull quantum git@github.com:openstack/quantum.git
-git_clone_or_pull horizon git@github.com:openstack/horizon.git
-git_clone_or_pull keystone git@github.com:openstack/keystone.git
+git_clone_or_pull openstack-common git@github.com:4P/openstack-common.git
+git_clone_or_pull nova git@github.com:4P/nova.git
+git_clone_or_pull swift git@github.com:4P/swift.git
+git_clone_or_pull glance git@github.com:4P/glance.git
+git_clone_or_pull quantum git@github.com:4P/quantum.git
+git_clone_or_pull horizon git@github.com:4P/horizon.git
+git_clone_or_pull keystone git@github.com:4P/keystone.git
+
+git_clone_or_pull django-openstack-auth git@github.com:4P/python-django-openstack-auth.git
+git_clone_or_pull nebula-dashboard git@github.com:4P/nebula-dashboard.git
+git_clone_or_pull zookeeper https://github.com/4P/zookeeper.git
+git_clone_or_pull maestro git@github.com:4P/maestro.git
+git_clone_or_pull tools git@github.com:4P/tools.git
+git_clone_or_pull eos git@github.com:4P/eos.git
+git_clone_or_pull nebulapki git@github.com:4P/nebulapki.git
+git_clone_or_pull initramfs-tools git@github.com:4P/initramfs-tools.git
+git_clone_or_pull ops git@github.com:4P/ops.git
+git_clone_or_pull python-maestroclient git@github.com:4P/python-maestroclient.git
+git_clone_or_pull sheepdog git@github.com:4P/sheepdog.git
 
 git_clone_or_pull openstack-qa https://github.com/openstack-dev/openstack-qa.git
 git_clone_or_pull openstack-ci https://github.com/openstack/openstack-ci.git
 git_clone_or_pull openstack-ci-puppet https://github.com/openstack/openstack-ci-puppet.git
 git_clone_or_pull tempest https://github.com/openstack/tempest.git
-git_clone_or_pull api-site-mock git@github.com:heckj/api-site-mock.git
 
 echo " ======================================= "
 echo "            GITHUB (related)             "
 echo " ======================================= "
 
+git_clone_or_pull featuredash git@github.com:4P/FeatureDash.git
 
 git_clone_or_pull compute-api https://github.com/openstack/compute-api.git
 git_clone_or_pull object-api https://github.com/openstack/object-api.git
@@ -114,13 +116,10 @@ git_clone_or_pull identity-api https://github.com/openstack/identity-api.git
 git_clone_or_pull openstack-manuals https://github.com/openstack/openstack-manuals.git
 git_clone_or_pull openstack-org https://github.com/toddmorey/openstack-org.git
 
-git_clone_or_pull django-openstack-auth git@github.com:4P/python-django-openstack-auth.git
-git_clone_or_pull featuredash git@github.com:4P/FeatureDash.git
 git_clone_or_pull py-statsd git@github.com:heckj/py-statsd.git
 git_clone_or_pull celery git://github.com/ask/celery.git
 git_clone_or_pull kombu https://github.com/ask/kombu.git
 git_clone_or_pull curator https://github.com/Netflix/curator
-git_clone_or_pull zookeeper https://github.com/apache/zookeeper.git
 git_clone_or_pull djeep https://github.com/cloudbuilders/djeep.git
 git_clone_or_pull crowbar git@github.com:heckj/crowbar.git
 git_clone_or_pull fabric git@github.com:fabric/fabric.git
