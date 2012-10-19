3834a36aa0a0881524d0f9927bba0ea29be4fee7
Joe Heck <heckj@mac.com>
2012-10-19 12:02:20 -0700

    new remotes

--- update.bash
+++ update.bash
@@ -3,6 +3,9 @@
 export USERNAME=$USER
 
 function git_clone_or_pull {
+# $1 == dirname
+# $2 == remotename
+# $3 == gitrepo
   echo " ------------------------------------------------------------------ "
   echo "src/$1"
   if [ -d ~/src/$1/.git ]; then
@@ -12,11 +15,14 @@ function git_clone_or_pull {
     git fetch --all --prune
   else
     cd ~/src
-    git clone $2 $1
+    git clone --recursive -o $2 $3 $1
   fi
 }
 
 function add_remote {
+# $1 == dirname
+# $2 == remotename
+# $3 == gitrepo
   echo " ------------------------------------------------------------------ "
   echo "src/$1"
   cd ~/src/$1
@@ -45,123 +51,119 @@ else
   git remote add devcamcar git@github.com:devcamcar/dotfiles.git
 fi
 
-git_clone_or_pull chef-ops git@github.com:4P/chef-ops.git
-git_clone_or_pull ipxe git@github.com:4P/ipxe.git
-git_clone_or_pull arista git@github.com:4P/arista.git
-git_clone_or_pull public-www git@github.com:4P/public-www.git
-git_clone_or_pull opencompute git@github.com:4P/opencompute.git
-git_clone_or_pull stashboard git@github.com:4P/stashboard.git
-git_clone_or_pull alexandria git@github.com:4P/alexandria.git
-git_clone_or_pull intranet git@github.com:4P/intranet.git
-git_clone_or_pull nebstack git@github.com:4P/nebstack.git
+git_clone_or_pull chef-ops 4P git@github.com:4P/chef-ops.git
+git_clone_or_pull ipxe 4P git@github.com:4P/ipxe.git
+git_clone_or_pull arista 4P git@github.com:4P/arista.git
+git_clone_or_pull public-www 4P git@github.com:4P/public-www.git
+git_clone_or_pull opencompute 4P git@github.com:4P/opencompute.git
+git_clone_or_pull stashboard 4P git@github.com:4P/stashboard.git
+git_clone_or_pull alexandria 4P git@github.com:4P/alexandria.git
+git_clone_or_pull intranet 4P git@github.com:4P/intranet.git
+git_clone_or_pull nebstack 4P git@github.com:4P/nebstack.git
 add_remote nebstack heckj git@github.com:heckj/nebstack.git
-git_clone_or_pull stackstack git@github.com:4P/stackstack.git
-git_clone_or_pull nebula_tools git@github.com:4P/nebula_tools.git
-git_clone_or_pull hardware_tools git@github.com:4P/hardware_tools.git
-git_clone_or_pull maestro git@github.com:4P/maestro.git
-git_clone_or_pull maestro-presence git@github.com:4P/maestro-presence.git
-git_clone_or_pull tools git@github.com:4P/tools.git
+git_clone_or_pull stackstack 4P git@github.com:4P/stackstack.git
+git_clone_or_pull nebula_tools 4P git@github.com:4P/nebula_tools.git
+git_clone_or_pull hardware_tools 4P git@github.com:4P/hardware_tools.git
+git_clone_or_pull maestro 4P git@github.com:4P/maestro.git
+git_clone_or_pull maestro-presence 4P git@github.com:4P/maestro-presence.git
+git_clone_or_pull tools 4P git@github.com:4P/tools.git
 add_remote tools heckj git@github.com:heckj/tools.git
-git_clone_or_pull nebulapki git@github.com:4P/nebulapki.git
-git_clone_or_pull initramfs-tools git@github.com:4P/initramfs-tools.git
-git_clone_or_pull ops git@github.com:4P/ops.git
-git_clone_or_pull python-maestroclient git@github.com:4P/python-maestroclient.git
-git_clone_or_pull lighthouse git@github.com:nebula/lighthouse.git
-git_clone_or_pull featuredash git@github.com:4P/FeatureDash.git
-git_clone_or_pull python-warlock git@github.com:4P/python-warlock.git
-git_clone_or_pull python-jsonschema git@github.com:4P/python-jsonschema.git
+git_clone_or_pull nebulapki 4P git@github.com:4P/nebulapki.git
+git_clone_or_pull initramfs-tools 4P git@github.com:4P/initramfs-tools.git
+git_clone_or_pull ops 4P git@github.com:4P/ops.git
+git_clone_or_pull python-maestroclient 4P git@github.com:4P/python-maestroclient.git
+git_clone_or_pull lighthouse nebula git@github.com:nebula/lighthouse.git
+git_clone_or_pull featuredash 4P git@github.com:4P/FeatureDash.git
+git_clone_or_pull python-warlock 4P git@github.com:4P/python-warlock.git
+git_clone_or_pull python-jsonschema 4P git@github.com:4P/python-jsonschema.git
 
 git_clone_or_pull cloudenvy git@github.com:heckj/cloudenvy.git
-add_remote cloudenvy bcwaldon https://github.com/bcwaldon/cloudenvy.git
+add_remote cloudenvy cloudenvy https://github.com/cloudenvy/cloudenvy.git
 
-git_clone_or_pull ocl git@github.com:4P/ocl.git
+git_clone_or_pull ocl 4P git@github.com:4P/ocl.git
 
-git_clone_or_pull packman-tools git@github.com:4P/packman-tools.git
+git_clone_or_pull packman-tools 4P git@github.com:4P/packman-tools.git
 
-git_clone_or_pull bp-issues https://github.com/ttx/bp-issues.git
-git_clone_or_pull devstack https://github.com/openstack-dev/devstack.git
+git_clone_or_pull bp-issues ttx https://github.com/ttx/bp-issues.git
+git_clone_or_pull devstack openstack https://github.com/openstack-dev/devstack.git
 add_remote devstack heckj git@github.com:heckj/devstack.git
 
-git_clone_or_pull python-keystoneclient git@github.com:4P/python-keystoneclient
+git_clone_or_pull python-keystoneclient 4P git@github.com:4P/python-keystoneclient
 add_remote python-keystoneclient openstack https://github.com/openstack/python-keystoneclient.git
 
-git_clone_or_pull python-quantumclient git@github.com:4P/python-quantumclient
+git_clone_or_pull python-quantumclient 4P git@github.com:4P/python-quantumclient
 add_remote python-quantumclient openstack https://github.com/openstack/python-quantumclient.git
 
-git_clone_or_pull python-novaclient git@github.com:4P/python-novaclient
+git_clone_or_pull python-novaclient 4P git@github.com:4P/python-novaclient
 add_remote python-novaclient openstack https://github.com/openstack/python-novaclient.git
 
-git_clone_or_pull python-glanceclient git@github.com:4P/python-glanceclient
+git_clone_or_pull python-glanceclient 4P git@github.com:4P/python-glanceclient
 add_remote python-glanceclient openstack https://github.com/openstack/python-glanceclient.git
 
-git_clone_or_pull python-swiftclient git@github.com:4P/python-swiftclient
+git_clone_or_pull python-swiftclient 4P git@github.com:4P/python-swiftclient
 add_remote python-swiftclient openstack https://github.com/openstack/python-swiftclient.git
 
-git_clone_or_pull noVNC git@github.com:4P/noVNC.git
+git_clone_or_pull noVNC 4P git@github.com:4P/noVNC.git
 add_remote noVNC openstack https://github.com/openstack/noVNC
 
-git_clone_or_pull openstack-common git@github.com:4P/openstack-common.git
+git_clone_or_pull openstack-common 4P git@github.com:4P/openstack-common.git
 add_remote openstack-common openstack https://github.com/openstack/openstack-common.git
 
-git_clone_or_pull python-openstackclient git@github.com:4P/python-openstackclient.git
+git_clone_or_pull python-openstackclient 4P git@github.com:4P/python-openstackclient.git
 add_remote python-openstackclient openstack https://github.com/openstack/python-openstackclient.git
 
-git_clone_or_pull python-cinderclient git@github.com:4P/python-cinderclient.git
+git_clone_or_pull python-cinderclient 4P git@github.com:4P/python-cinderclient.git
 add_remote python-cinderclient openstack https://github.com/openstack/python-cinderclient.git
 
-git_clone_or_pull nova git@github.com:4P/nova.git
+git_clone_or_pull nova 4P git@github.com:4P/nova.git
 add_remote nova openstack https://github.com/openstack/nova.git
 
-git_clone_or_pull swift git@github.com:4P/swift.git
+git_clone_or_pull swift 4P git@github.com:4P/swift.git
 add_remote swift openstack https://github.com/openstack/swift.git
 
-git_clone_or_pull cinder git@github.com:4P/cinder.git
+git_clone_or_pull cinder 4P git@github.com:4P/cinder.git
 add_remote cinder openstack https://github.com/openstack/cinder.git
 
-git_clone_or_pull glance git@github.com:4P/glance.git
+git_clone_or_pull glance 4P git@github.com:4P/glance.git
 add_remote glance openstack https://github.com/openstack/glance.git
 
-git_clone_or_pull quantum git@github.com:4P/quantum.git
+git_clone_or_pull quantum 4P git@github.com:4P/quantum.git
 add_remote quantum openstack https://github.com/openstack/quantum.git
 
-git_clone_or_pull horizon git@github.com:4P/horizon.git
+git_clone_or_pull horizon 4P git@github.com:4P/horizon.git
 add_remote horizon openstack https://github.com/openstack/horizon.git
 
-git_clone_or_pull keystone git@github.com:4P/keystone.git
+git_clone_or_pull keystone 4P git@github.com:4P/keystone.git
 add_remote keystone openstack https://github.com/openstack/keystone.git
 
-git_clone_or_pull django-openstack-auth git@github.com:4P/python-django-openstack-auth.git
-git_clone_or_pull nebula-dashboard git@github.com:4P/nebula-dashboard.git
-git_clone_or_pull zookeeper git@github.com:4P/zookeeper.git
-git_clone_or_pull sheepdog git@github.com:4P/sheepdog.git
-git_clone_or_pull eos git@github.com:4P/eos.git
+git_clone_or_pull django-openstack-auth 4P git@github.com:4P/python-django-openstack-auth.git
+git_clone_or_pull nebula-dashboard 4P git@github.com:4P/nebula-dashboard.git
+git_clone_or_pull zookeeper 4P git@github.com:4P/zookeeper.git
+git_clone_or_pull sheepdog 4P git@github.com:4P/sheepdog.git
+git_clone_or_pull eos 4P git@github.com:4P/eos.git
 
-git_clone_or_pull openstack-qa https://github.com/openstack-dev/openstack-qa.git
-git_clone_or_pull openstack-ci https://github.com/openstack/openstack-ci.git
-git_clone_or_pull openstack-ci-puppet https://github.com/openstack/openstack-ci-puppet.git
-git_clone_or_pull tempest git@github.com:4P/tempest.git
+git_clone_or_pull openstack-qa openstack https://github.com/openstack-dev/openstack-qa.git
+git_clone_or_pull openstack-ci openstack https://github.com/openstack/openstack-ci.git
+git_clone_or_pull openstack-ci-puppet openstack https://github.com/openstack/openstack-ci-puppet.git
+git_clone_or_pull tempest 4P git@github.com:4P/tempest.git
 add_remote tempest openstack https://github.com/openstack/tempest.git
 
-git_clone_or_pull compute-api https://github.com/openstack/compute-api.git
-git_clone_or_pull object-api https://github.com/openstack/object-api.git
-git_clone_or_pull image-api https://github.com/openstack/image-api.git
-git_clone_or_pull identity-api https://github.com/openstack/identity-api.git
-git_clone_or_pull openstack-manuals https://github.com/openstack/openstack-manuals.git
-git_clone_or_pull openstack-org https://github.com/toddmorey/openstack-org.git
+git_clone_or_pull compute-api openstack https://github.com/openstack/compute-api.git
+git_clone_or_pull object-api openstack https://github.com/openstack/object-api.git
+git_clone_or_pull image-api openstack https://github.com/openstack/image-api.git
+git_clone_or_pull identity-api openstack https://github.com/openstack/identity-api.git
+git_clone_or_pull openstack-manuals openstack https://github.com/openstack/openstack-manuals.git
+git_clone_or_pull openstack-org toddmorey https://github.com/toddmorey/openstack-org.git
 
-git_clone_or_pull py-statsd git@github.com:heckj/py-statsd.git
-git_clone_or_pull celery git://github.com/ask/celery.git
-git_clone_or_pull kombu https://github.com/ask/kombu.git
-git_clone_or_pull curator https://github.com/Netflix/curator
-git_clone_or_pull djeep https://github.com/cloudbuilders/djeep.git
-git_clone_or_pull crowbar git@github.com:heckj/crowbar.git
-git_clone_or_pull fabric git@github.com:fabric/fabric.git
-git_clone_or_pull kong https://github.com/cloudbuilders/kong.git
-git_clone_or_pull stacktester https://github.com/rackspace-titan/stacktester.git
-git_clone_or_pull benchmarker git@github.com:heckj/benchmarker.git
-git_clone_or_pull doyen git@github.com:heckj/doyen.git
-git_clone_or_pull om git@github.com:heckj/om.git
-git_clone_or_pull zombie git@github.com:termie/zombie.git
+git_clone_or_pull py-statsd heckj git@github.com:heckj/py-statsd.git
+git_clone_or_pull celery ask git://github.com/ask/celery.git
+git_clone_or_pull kombu ask https://github.com/ask/kombu.git
+git_clone_or_pull curator netflix https://github.com/Netflix/curator
+git_clone_or_pull fabric fabric git@github.com:fabric/fabric.git
+git_clone_or_pull benchmarker heckj git@github.com:heckj/benchmarker.git
+git_clone_or_pull doyen heckj git@github.com:heckj/doyen.git
+git_clone_or_pull om heckj git@github.com:heckj/om.git
+git_clone_or_pull zombie termie git@github.com:termie/zombie.git
 
 # export maestro latest for reading offline
 mkdir -p ~/Dropbox/nebula/src/maestro
