ee6e183aa0a0d834a4efca71a7f18fd7fd08c428
Joe Heck <heckj@mac.com>
2012-10-28 10:24:16 -0700

    adding python-virtualenv

--- packagework.sh
+++ packagework.sh
@@ -1,5 +1,6 @@
 sudo pip install -U git-sweep
 sudo pip install -U git-review
+sudo apt-get install python-virtualenv
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y javahelper pbuilder
