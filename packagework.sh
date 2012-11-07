0302eacf49de4baa8084f4b10f6fca588e8cf8ca
Joe Heck <heckj@mac.com>
2012-11-07 09:05:36 -0800

    extended to cover keystone dependencies

--- packagework.sh
+++ packagework.sh
@@ -1,7 +1,13 @@
 sudo pip install -U git-sweep
 sudo pip install -U git-review
-sudo apt-get install python-virtualenv
+# keystone development pieces...
+sudo apt-get install -y python-virtualenv python-dev libxslt1-dev
+sudo apt-get install -y libldap2-dev libsasl2-dev
+# general 'make sure it's up to date'
 sudo apt-get update
 sudo apt-get dist-upgrade -y
+# include bits for debian/ubuntu packaging
 sudo apt-get install -y javahelper pbuilder
+sudo apt-get autoremove -y
+# pre-cache the pbuilder debbootstrap image
 sudo pbuilder create
