59bddc465b253efdf5dafb40b56a18a605db404c
Joe Heck <heckj@mac.com>
2013-01-24 20:45:18 +0000

    adding dh-make to packagemaker

--- packagework.sh
+++ packagework.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y libldap2-dev libsasl2-dev
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 # include bits for debian/ubuntu packaging
-sudo apt-get install -y javahelper pbuilder
+sudo apt-get install -y javahelper pbuilder dh-make
 sudo apt-get autoremove -y
 # pre-cache the pbuilder debbootstrap image
 sudo pbuilder create
