e2dc6332eef4a7cf0d8518119c8940a98552532a
Joe Heck <heckj@mac.com>
2013-06-13 12:45:36 -0700

    adding gabe's javascript client code

--- update.bash
+++ update.bash
@@ -195,6 +195,9 @@ git_clone_or_pull identity-api openstack https://github.com/openstack/identity-a
 git_clone_or_pull openstack-manuals openstack https://github.com/openstack/openstack-manuals.git
 git_clone_or_pull openstack-org toddmorey https://github.com/toddmorey/openstack-org.git
 
+git_clone_or_pull js-maestroclient nebula https://github.com/nebula/js-maestroclient.git
+git_clone_or_pull js-openclient gabrielhurley git@github.com:gabrielhurley/js-openclient.git
+
 git_clone_or_pull fabric fabric git@github.com:fabric/fabric.git
 git_clone_or_pull benchmarker heckj git@github.com:heckj/benchmarker.git
 git_clone_or_pull doyen heckj git@github.com:heckj/doyen.git
